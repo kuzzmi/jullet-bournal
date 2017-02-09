1420edcbe30a38c8a55993fe21f6ca32f0effcc5
Igor Kuzmenko <igor@kuzzmi.com>
2017-02-09 20:51:46 +0100

    Adds reset header block style on enter. Adds pages

--- src/App.js
+++ src/App.js
@@ -3,14 +3,68 @@ import './App.css';
 
 import Editor from './Editor.js';
 
+import { EditorState } from 'draft-js';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Editor />
-      </div>
-    );
-  }
+    state = {
+        pages: {
+            1: EditorState.createEmpty(),
+        },
+        pageId: 1,
+    };
+
+    getPage = () => this.state.pages[this.state.pageId];
+
+    onPrevPage = () => this.setState({ pageId: this.state.pageId - 1 });
+
+    onNextPage = () => this.setState({ pageId: this.state.pageId + 1 });
+
+    savePage = ({ editorState }) => {
+        const { pageId, pages } = this.state;
+        this.setState({
+            pages: {
+                ...pages,
+                [pageId]: editorState,
+            },
+        });
+    }
+
+    goToPage = pageId => this.setState({ pageId });
+
+    renderPages = () => {
+        const { pageId, pages } = this.state;
+
+        return (
+            <ul>
+            { Object.keys(pages).map((id, i) => {
+                return (
+                    <li key={ i } onClick={ () => this.goToPage(id) }>
+                    { pageId === id && '>' } Note #{ i }
+                    </li>
+                );
+            })}
+            </ul>
+        );
+    }
+
+    render() {
+        const page = this.getPage();
+        return (
+            <div className="App">
+                <div>
+                    <div>
+                        <button onClick={ this.onPrevPage }>Prev</button>
+                        <button onClick={ this.onNextPage }>Next</button>
+                    </div>
+                    { this.renderPages() }
+                </div>
+                <Editor
+                    page={ page }
+                    onChange={ this.savePage }
+                    />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Editor.js
+++ src/Editor.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import { Editor, EditorState, RichUtils } from 'draft-js';
+import {
+    Editor,
+    EditorState,
+    RichUtils,
+    Modifier,
+} from 'draft-js';
 import './Editor.css';
 
 function blockStyleFn(contentBlock) {
@@ -41,10 +46,10 @@ const BLOCK_TYPES = [
     { label: 'H1', style: 'header-one' },
     { label: 'H2', style: 'header-two' },
     { label: 'H3', style: 'header-three' },
-    { label: 'Blockquote', style: 'blockquote' },
-    { label: 'UL', style: 'unordered-list-item' },
-    { label: 'OL', style: 'ordered-list-item' },
-    { label: 'Code Block', style: 'code-block' },
+    { label: '"', style: 'blockquote' },
+    { label: '*', style: 'unordered-list-item' },
+    { label: '1.', style: 'ordered-list-item' },
+    { label: '</>', style: 'code-block' },
 ];
 
 const BlockStyleControls = (props) => {
@@ -56,11 +61,11 @@ const BlockStyleControls = (props) => {
         .getType();
 
     return (
-          <div className="block-tools">
+          <div className="tools">
             {BLOCK_TYPES.map((type) =>
               <StyleButton
                 key={type.label}
-                active={type.style === blockType}
+                isActive={type.style === blockType}
                 label={type.label}
                 onToggle={props.onToggle}
                 style={type.style}
@@ -70,27 +75,47 @@ const BlockStyleControls = (props) => {
     );
 };
 
+function insertNewline(editorState) {
+    var contentState = Modifier.splitBlock(
+        editorState.getCurrentContent(),
+        editorState.getSelection()
+    );
+    return EditorState.push(editorState, contentState, 'split-block');
+}
+
 export default class extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            editorState: EditorState.createEmpty()
+            editorState: props.page || EditorState.createEmpty()
         };
 
-        this.onChange = (editorState, focus) => this.setState({
-            editorState
-        }, () => {
-            setTimeout(() => this.refs.editor.focus(), 0);
-        });
+        this.onChange = (editorState, focus) => {
+            props.onChange({ editorState });
+            this.setState({
+                editorState
+            }, () => {
+                setTimeout(() => this.refs.editor.focus(), 0);
+            });
+        };
+
         this.handleKeyCommand = this.handleKeyCommand.bind(this);
-        this.toggleBlockType = this.toggleBlockType.bind(this);
-        this.onTab = this.onTab.bind(this);
+        this.toggleBlockType  = this.toggleBlockType.bind(this);
+        this.onTab            = this.onTab.bind(this);
+        this.onReturn         = this.onReturn.bind(this);
+    }
+
+    componentWillReceiveProps({ page }) {
+        if (page !== this.props.page) {
+            this.onChange(page || EditorState.createEmpty());
+        }
     }
 
     handleKeyCommand(command) {
+        const { editorState } = this.state;
         console.log(command);
-        const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+        const newState = RichUtils.handleKeyCommand(editorState, command);
         if (newState) {
             this.onChange(newState);
             return 'handled';
@@ -101,6 +126,23 @@ export default class extends Component {
     onTab(e) {
         const maxDepth = 4;
         this.onChange(RichUtils.onTab(e, this.state.editorState, maxDepth));
+    }
+
+    onReturn(e) {
+        const { editorState } = this.state;
+        const block = RichUtils.getCurrentBlockType(editorState);
+
+        // reset block style if it was a header
+        if (block.indexOf('header') === 0) {
+            e.preventDefault();
+            const cleanEditor = RichUtils.toggleBlockType(insertNewline(editorState), block);
+            this.onChange(cleanEditor);
+            return 'handled';
+        }
+
+        // TODO: clean 'list' style if the bullet is empty (only bullet)
+
+        return 'not-handled';
     }
 
     toggleBlockType(type) {
@@ -124,6 +166,7 @@ export default class extends Component {
                     handleKeyCommand={ this.handleKeyCommand }
                     blockStyleFn={ blockStyleFn }
                     onChange={ this.onChange }
+                    handleReturn={ this.onReturn }
                     onTab={ this.onTab }
                     />
             </div>
